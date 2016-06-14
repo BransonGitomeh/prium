387bb1043e5189dea05abd8759407e3347fa77b8
BransonGitomeh <sirbranson@outlook.com>
2016-06-14 23:14:13 +0300

    fx the example thing

--- start.js
+++ start.js
@@ -26,7 +26,7 @@ const contactPoint2 = process.env.OPENSHIFT_CASSANDRA_DB_HOST + ":" + process.en
 
 var connectionOptions = {
    contactPoints:[contactPoint1,contactPoint2],
-   keyspace: 'awesome'
+   keyspace: 'examples'
  };
 
 var client = new cassandraDriver.Client(connectionOptions);
