649041a6045cc13ab44d1469b770c57db0eaeeb8
BransonGitomeh <sirbranson@outlook.com>
2016-06-14 11:07:50 +0300

    rmoved self cassandra and added catridge

--- start.js
+++ start.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function(req, res) {
+	// 	OPENSHIFT_CASSANDRA_HOST      The Cassandra IP address
+	// OPENSHIFT_CASSANDRA_PORT      The Cassandra port
+	// OPENSHIFT_CASSANDRA_LOG_DIR     The path to the Cassandra log directory
+	// OPENSHIFT_CASSANDRA_NATIVE_TRANSPORT_PORT Port for connecting with java client, on NATIVE TRANSPORT
+	res.send({
+		cassandra_host:process.env.OPENSHIFT_CASSANDRA_HOST
+	})
 });
 
 const port = process.env.OPENSHIFT_NODEJS_PORT || 4000
 const ip = process.env.OPENSHIFT_NODEJS_IP || "localhost"
 
 app.listen(port, ip, 511, function(err) {
-  console.log('listening on *:' + port);
+	console.log('listening on *:' + port);
 });
